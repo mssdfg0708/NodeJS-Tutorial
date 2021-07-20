78853827cba8390c04e0834060ef7a36080a7abd
mssdfg0708 <mssdfg0708@naver.com>
2021-07-20 18:34:02 +0900

    기능 구현 'File Create'

--- Nodejs/main.js
+++ Nodejs/main.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const qs = require('querystring');
 
 function paintHtml(title, fileList, body) {
   return `
@@ -12,6 +13,7 @@ function paintHtml(title, fileList, body) {
   <body>
     <h1><a href="/">WEB</a></h1>
     ${fileList}
+    <a href = "/create">create</a>
     ${body}
   </body>
   </html>
@@ -33,9 +35,6 @@ const app = http.createServer((request,response) => {
   const pathname = reqUrl.pathname;
   const targetDir = '../Data';
   let title = reqUrl.searchParams.get('id');
-
-  console.log(title);
-  console.log(pathname);
   
   if(pathname === '/'){
     if (title === null) {
@@ -58,7 +57,41 @@ const app = http.createServer((request,response) => {
         })
       });
     }
-  } 
+  }
+  else if(pathname === '/create') {
+    fs.readdir(targetDir, (error, files) => {
+      title = 'WEB1 - create';
+      const fileList = makeFileList(files);
+      const template = paintHtml(title, fileList, `
+      <form action="http://localhost:3000/createProcess" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+      `);
+      response.writeHead(200);
+      response.end(template);
+    });
+  }
+  else if (pathname === '/createProcess') {
+    let body = '';
+    request.on('data', (data) => {
+        body += data;
+    });
+    request.on('end', () => {
+      const post = qs.parse(body);
+      const title = post.title;
+      const description = post.description;
+      fs.writeFile(`../Data/${title}`, description, 'utf8', (err) => {
+        response.writeHead(302, {Location: `/?id=${title}`});
+        response.end();
+      })
+    });
+  }
   else {
     response.writeHead(404);
     response.end('Not found');
