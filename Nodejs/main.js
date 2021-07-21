3f4142811bcb5bcbf32ac423ecd42810a5c3ce6b
mssdfg0708 <mssdfg0708@naver.com>
2021-07-21 16:56:56 +0900

    기능 구현 'File Update'

--- Nodejs/main.js
+++ Nodejs/main.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const qs = require('querystring');
 
-function paintHtml(title, fileList, body) {
+function paintHtml(title, fileList, body, control) {
   return `
   <!doctype html>
   <html>
@@ -13,7 +13,7 @@ function paintHtml(title, fileList, body) {
   <body>
     <h1><a href="/">WEB</a></h1>
     ${fileList}
-    <a href = "/create">create</a>
+    ${control}
     ${body}
   </body>
   </html>
@@ -42,7 +42,9 @@ const app = http.createServer((request,response) => {
         title = 'Welcome';
         const description = 'Hello Nodejs';
         const fileList = makeFileList(files);
-        const template = paintHtml(title, fileList, `<h2>${title}</h2><p>${description}</p>`);
+        const template = paintHtml(title, fileList, 
+          `<h2>${title}</h2><p>${description}</p>`,
+          `<a href = "/create">create</a>`);
         response.writeHead(200);
         response.end(template);
       })
@@ -51,7 +53,9 @@ const app = http.createServer((request,response) => {
       fs.readdir(targetDir, (error, files) => {
         const fileList = makeFileList(files);
         fs.readFile(`../Data/${title}`, 'utf8', (err, description) => {
-          const template = paintHtml(title, fileList, `<h2>${title}</h2><p>${description}</p>`);
+          const template = paintHtml(title, fileList, 
+            `<h2>${title}</h2><p>${description}</p>`,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
           response.writeHead(200);
           response.end(template);
         })
@@ -63,16 +67,16 @@ const app = http.createServer((request,response) => {
       title = 'WEB1 - create';
       const fileList = makeFileList(files);
       const template = paintHtml(title, fileList, `
-      <form action="http://localhost:3000/createProcess" method="post">
+      <form action="/createProcess" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
         <p>
           <textarea name="description" placeholder="description"></textarea>
         </p>
         <p>
-          <input type="submit">
+          <input type="submit", value = "save">
         </p>
       </form>
-      `);
+      `, '');
       response.writeHead(200);
       response.end(template);
     });
@@ -91,6 +95,48 @@ const app = http.createServer((request,response) => {
         response.end();
       })
     });
+  }
+  else if(pathname === '/update'){
+    fs.readdir(targetDir, function(error, files){
+      fs.readFile(`../Data/${title}`, 'utf8', function(err, description){
+        title = reqUrl.searchParams.get('id');
+        const fileList = makeFileList(files);
+        const template = paintHtml(title, fileList,
+          `
+          <form action="/updateProcess" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+            <p>
+              <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit",  value = "save">
+            </p>
+          </form>
+          `, ''
+        );
+        response.writeHead(200);
+        response.end(template);
+      });
+    });
+  }
+  else if(pathname === '/updateProcess') {
+    let body = '';
+    request.on('data', (data) => {
+        body += data;
+    });
+    request.on('end', () => {
+      const post = qs.parse(body);
+      const id = post.id;
+      const title = post.title;
+      const description = post.description;
+      fs.rename(`../Data/${id}`, `../Data/${title}`, (err) => {
+        fs.writeFile(`../Data/${title}`, description, 'utf8', (err) => {
+          response.writeHead(302, {Location: `/?id=${title}`});
+          response.end();
+        })
+      })
+    })
   }
   else {
     response.writeHead(404);
