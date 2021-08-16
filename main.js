d58469c2ccd9168b63be2bcbd48cb7b07f59f93f
mssdfg0708 <mssdfg0708@naver.com>
2021-08-16 21:18:47 +0900

    기능 구현 '저자 생성 수정 삭제'

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const http = require('http');
+const { authorSelect, authorTable } = require('./lib/template');
 const topic = require('./lib/topic')
- 
+const author = require('./lib/author');
+const path = require('path');
+  
 const app = http.createServer((request,response) => {
   console.log("----Catch Requset----")
   const baseURL = 'http://' + request.headers.host + '/';
@@ -16,15 +19,24 @@ const app = http.createServer((request,response) => {
     }
   } else if(pathname === '/create') {
     topic.create(request, response);
-  } else if (pathname === '/createProcess') {
+  } else if(pathname === '/createProcess') {
     topic.createProcess(request, response);
   } else if(pathname === '/update'){
     topic.update(request, response);
   } else if(pathname === '/updateProcess') {
     topic.updateProcess(request, response);
-  }
-  else if(pathname === '/deleteProcess') {
+  } else if(pathname === '/deleteProcess') {
     topic.deleteProcess(request, response);
+  } else if(pathname === '/author') {
+    author.home(request, response);
+  } else if(pathname === '/author/createProcess') {
+    author.createProcess(request, response);
+  } else if(pathname === '/author/update') {
+    author.update(request, response);
+  } else if(pathname === '/author/updateProcess') {
+    author.updateProcess(request, response);
+  } else if(pathname === '/author/deleteProcess') {
+    author.deleteProcess(request, response);
   }
   else {
     response.writeHead(404);
